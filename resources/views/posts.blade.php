50d4d7e56b4c2ea8e496c60ce339084e096fe1ed
Sam Hollyer <rsamhollyer@gmail.com>
2022-04-26 13:19:25 -0500

    updates

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,44 +3,14 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card></x-post-featured-card>
 
-        <div class="lg:grid lg:grid-cols-2">
+        @if ($posts->count())
+            <x-post-grid :posts="$posts" />
+        @else
+            <p class="text-center text-3xl">No posts yet. Please check back later.</p>
+        @endif
 
-            <x-post-card> </x-post-card>
-            <x-post-card> </x-post-card>
 
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-
-            <x-post-card> </x-post-card>
-            <x-post-card> </x-post-card>
-            <x-post-card> </x-post-card>
-
-        </div>
     </main>
-    {{-- @foreach ($posts as $post)
-        <article>
-            <a href="/posts/{{ $post->slug }}">
-                <h1>{{ $post->title }}</h1>
-            </a>
 
-            <span>
-                By
-                <a href="/authors/{{ $post->author->username }}">
-                    {{ $post->author->name }}
-                </a>
-                in
-                <a href="/categories/{{ $post->category->slug }}">
-                    {{ $post->category->name }}
-                </a>
-            </span>
-
-            <div>
-                {{ $post->excerpt }}
-            </div>
-
-        </article>
-    @endforeach --}}
 </x-layout>
