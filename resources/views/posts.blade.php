9c02fa75491a8dc3c41659f6d325af5a09bbc9a6
Sam Hollyer <rsamhollyer@gmail.com>
2022-04-26 11:36:19 -0500

    updates

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,26 @@
-{{-- @extends('layout')
-
-@section('content')
-    @foreach ($posts as $post) --}}
-{{-- <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <a href="/posts/{{ $post->slug }}">
-                <h1>{{ $post->title }}</h1>
-            </a>
-
-            <div>
-                {{ $post->excerpt }}
-            </div>
-
-        </article>
-    @endforeach
-@endsection --}}
-{{-- This section above does not take advantage of the blade component syntax --}}
-
-
 <x-layout>
-    @foreach ($posts as $post)
+
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card></x-post-featured-card>
+
+        <div class="lg:grid lg:grid-cols-2">
+
+            <x-post-card> </x-post-card>
+            <x-post-card> </x-post-card>
+
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+
+            <x-post-card> </x-post-card>
+            <x-post-card> </x-post-card>
+            <x-post-card> </x-post-card>
+
+        </div>
+    </main>
+    {{-- @foreach ($posts as $post)
         <article>
             <a href="/posts/{{ $post->slug }}">
                 <h1>{{ $post->title }}</h1>
@@ -40,5 +42,5 @@
             </div>
 
         </article>
-    @endforeach
+    @endforeach --}}
 </x-layout>
