2d0e735dd4bd4c867ada7587584a030c6bad9b9b
Sam Hollyer <rsamhollyer@gmail.com>
2022-04-28 11:27:46 -0500

    updates

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,9 +21,12 @@ class Post extends Model {
     $query->when(
       $filters['search'] ?? false,
       fn ($query, $search) =>
-      $query
-        ->where('title', 'like', "%{$search}%")
-        ->orWhere('body', 'like', "%{$search}%")
+      $query->where(
+        fn ($query) =>
+        $query
+          ->where('title', 'like', "%{$search}%")
+          ->orWhere('body', 'like', "%{$search}%")
+      )
     );
 
     $query->when(
