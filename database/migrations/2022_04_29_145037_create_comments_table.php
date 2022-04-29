5c3f14d45555e4eaf3d1ccb8f8314ba9edbc885c
Sam Hollyer <rsamhollyer@gmail.com>
2022-04-29 10:01:32 -0500

    updates

--- database/migrations/2022_04_29_145037_create_comments_table.php
+++ database/migrations/2022_04_29_145037_create_comments_table.php
@@ -4,28 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('comments', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+      $table->text('body');
+      $table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('comments');
-    }
+      // $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete(); // Long way
+
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('comments');
+  }
 };
